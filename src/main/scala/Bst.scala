04c0b4aec0decaf3ad4a86b18caa947282fcbcaf
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-20 15:48:22 +0100

    refactor

--- src/main/scala/Bst.scala
+++ src/main/scala/Bst.scala
@@ -2,26 +2,15 @@ import scala.annotation.tailrec
 
 case class Bst[T](value: T, left: Option[Bst[T]], right: Option[Bst[T]]):
    def insert(v: T): Bst[T] =
-      if v.hashCode <= value.hashCode
-      then Bst(value, Some(left.map(_.insert(v)).getOrElse(Bst(v))), right)
-      else Bst(value, left, Some(right.map(_.insert(v)).getOrElse(Bst(v))))
-
-   def toList: List[T] =
-      left
-         .map(_.toList)
-         .getOrElse(Nil)
-         .concat(List(value))
-         .concat(right.map(_.toList).getOrElse(Nil))
+      if v.## <= value.##
+      then copy(left = Some(left.map(_.insert(v)).getOrElse(Bst(v))))
+      else copy(right = Some(right.map(_.insert(v)).getOrElse(Bst(v))))
 
 object Bst:
    def apply[T](v: T): Bst[T] = Bst(v, None, None)
 
    def fromList[T](l: List[T]): Bst[T] =
-      @tailrec
-      def insertAll(bst: Bst[T], l: List[T]): Bst[T] =
-         l match
-            case Nil            => bst
-            case ::(head, tail) => insertAll(bst.insert(head), tail)
-      insertAll(Bst(l.head), l.tail)
+      l.tail.foldLeft(Bst(l.head))((tree, value) => tree.insert(value))
 
-   def toList[T](bst: Bst[T]): List[T] = bst.toList
+   def toList[T](bst: Bst[T]): List[T] =
+      bst.left.toList.flatMap(toList) ::: bst.value :: bst.right.toList.flatMap(toList)
